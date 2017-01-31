8192c33ca0e12fd88f02cc024ade1f5a6b378ff4
Jerker Bäckström <jerker.backstrom@outlook.com>
2017-01-31 22:14:24 +0200

    Added method for fetching album data to AlbumList component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then(data => this.setState({ albums: data }));
+  }
+
   render() {
     return (
       <View>
